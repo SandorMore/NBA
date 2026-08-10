d5a0e52d6cb54542facee5e7a93a642c82cf344e
Saso <msaso0725@gmail.com>
2026-08-10 21:27:48 +0200

    Remount

--- NBA/src/main.rs
+++ NBA/src/main.rs
@@ -1,6 +1,17 @@
-
+#[allow(unused)]
+#[macro_use] extern crate rocket;
 
 #[rocket::main]
-async fn main() {
-    
+async fn main() -> ()
+{
+    rocket::build()
+        .mount("/", routes![frontPage])
+        .launch()
+        .await;
 }
+
+#[get("/")]
+fn frontPage() -> &'static str
+{
+    return "hello world";
+}   
